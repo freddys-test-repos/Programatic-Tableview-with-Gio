1a6ca1ce2197a256a414a11c2e59c9119930f598
amerino <alfimerino@gmail.com>
2019-04-09 22:46:09 -0400

    2nd commit

--- GIO/ViewController.swift
+++ GIO/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    let names = ["Ivan", "Nilson", "Gio", "Ryan", "Tommy"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let newViewController = ViewController()
+        newViewController.title = "Swift Miami Group"
+        navigationController?.pushViewController(newViewController, animated: true)
+    }
+    
 }
 
